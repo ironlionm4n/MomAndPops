ec4e7ffac273d28f8cadce4a478d337272f77c07
rautry6 <94204738+rautry6@users.noreply.github.com>
2021-11-15 15:33:34 -0500

    Testing Comit

---
+++ MomAndPops/Payment.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace MomAndPops
+{
+    partial class Payment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Payment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(229)))), ((int)(((byte)(204)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Payment";
+            this.Text = "Payment";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ MomAndPops/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MomAndPops
+{
+    public partial class Payment : Form
+    {
+        public Payment()
+        {
+            InitializeComponent();
+        }
+    }
+}
